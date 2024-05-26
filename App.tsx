97a028df811d76df947d3a608918f0f7c775021f
Volodymyr Sinelnikov <volodymyr.sinelnikov@pettersonapps.com>
2024-05-26 21:32:28 +0300

    added theme, splashscreen

--- App.tsx
+++ App.tsx
@@ -1,14 +1,48 @@
+import { useFonts } from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect } from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+SplashScreen.preventAutoHideAsync();
+
+export const App = () => {
+
+  const [fontsLoaded] = useFonts({
+    'Montserrat-SemiBold': require('./src/assets/fonts/Montserrat-SemiBold.ttf'),
+    'Montserrat-Medium': require('./src/assets/fonts/Montserrat-Medium.ttf'),
+    'Montserrat-Regular': require('./src/assets/fonts/Montserrat-Regular.ttf'),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return (
+      <View style={{ flex: 1 }}>
+        <Image
+          source={require("./src/assets/images/splashScreen.png")}
+          style={{
+            width: "100%",
+            height: "100%",
+          }}
+        />
+      </View>
+    );
+  } else {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
---
+++ src/theme/colors.ts
@@ -0,0 +1,16 @@
+const colors = {
+  dark: "#1F1D2B",
+  soft: "#252836",
+  blue: "#12CDD9",
+  green: "#22B07D",
+  orange: "#FF8700",
+  red: "#FF7256",
+  black: "#171725",
+  darkGrey: "#696974",
+  grey: "#92929D",
+  whiteGrey: "#F1F1F5",
+  white: "#FFF",
+  lineDark: "#EAEAEA",
+};
+
+export default colors;
